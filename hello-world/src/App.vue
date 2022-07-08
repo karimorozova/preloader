ec3005cfd7f40445feb0349c3c80d798dca09e67
karimorozova <morozova.karine13@gmail.com>
2022-07-08 11:37:30 +0300

    Create Loader Component

---
+++ hello-world/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <MyPreloader />
+  </div>
+
+</template>
+
+<script>
+import MyPreloader from '@/components/MyPreloader'
+
+export default {
+  name: 'App',
+  components: {
+    MyPreloader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
